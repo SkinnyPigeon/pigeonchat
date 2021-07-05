c6e01bbc3b8923a04778416aed2ec653cd1fa8fd
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2021-07-05 12:43:04 +0200

    removed unused ref

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Grid, Typography, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
